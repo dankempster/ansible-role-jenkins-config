1c02d96f3346b1c976832db800c956350b76a97f
Dan Kempster <me@dankempster.co.uk>
2019-01-13 11:24:55 +0000

    Test Jenkins config files exist
    Really I need to check the contents of these files!

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,36 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_jenkins_is_configured(host):
+    f = host.file('/var/lib/jenkins/config.xml')
+
+    assert f.exists
+    assert f.user == 'jenkins'
+    assert f.group == 'jenkins'
+    assert f.mode == 0644
+
+def test_jenkins_location_is_configured(host):
+    f = host.file('/var/lib/jenkins/jenkins.model.JenkinsLocationConfiguration.xml')
+
+    assert f.exists
+    assert f.user == 'jenkins'
+    assert f.group == 'jenkins'
+    assert f.mode == 0644
+
+def test_jenkins_CLI_is_configured(host):
+    f = host.file('/var/lib/jenkins/jenkins.CLI.xml')
+
+    assert f.exists
+    assert f.user == 'jenkins'
+    assert f.group == 'jenkins'
+    assert f.mode == 0644
+
+
+def test_jenkins_Shell_is_configured(host):
+    f = host.file('/var/lib/jenkins/hudson.tasks.Shell.xml')
+
+    assert f.exists
+    assert f.user == 'jenkins'
+    assert f.group == 'jenkins'
+    assert f.mode == 0644
